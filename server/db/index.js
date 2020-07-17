16548e9b155f84819fbe7c7ab2b1ab7d5c96edde
Coul Greer <coulgreer1@hotmail.com>
2020-07-17 03:33:13 -0400

    Setup environment for Travis-CI.
    This is mainly for automating tests for the API.

---
+++ server/db/index.js
@@ -0,0 +1,10 @@
+const { Pool } = require("pg");
+
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool();
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
+
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is listening to port ${port}`));
+getChampionIds();
 
-app.get("/testRoute", (req, res) =>
+// ROUTES //
+app.get("/testRoute", cors(), (req, res) =>
   res.json({ message: "Hello, world!! I am a server :-)" })
 );
+
+// FUNCTIONS //
+function getChampionIds() {
+  pool
+    .query("SELECT champion_id FROM champion")
+    .then((res) => console.log(res.rows))
+    .catch((err) => console.log(err));
+}
