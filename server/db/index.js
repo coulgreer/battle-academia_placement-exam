eb13aa264731f8bf8a0deed8ed57ba484aa534e0
cagreer18 <coulgreer1@hotmail.com>
2020-08-18 00:54:41 -0400

    Removed Docker out of client and moved Newman outside container.

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,7 @@
 const { Pool } = require("pg");
 
+
+// Test to see if actually connected to database. If not, retry at least 5 times.
 const pool = process.env.DATABASE_URL
   ? new Pool({
       connectionString: process.env.DATABASE_URL,
